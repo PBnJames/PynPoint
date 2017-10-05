8dd7cec1704045e0a7b2b5c03b616bdb5b84057a
PBnJames <jamesmmoore@ufl.edu>
2017-10-05 18:53:59 -0400

    fixed issues with settingsactivitybuttons, need to fix text colors

--- app/src/main/java/com/example/android/pynpoint/SettingsActivity.java
+++ app/src/main/java/com/example/android/pynpoint/SettingsActivity.java
@@ -1,13 +1,34 @@
 package com.example.android.pynpoint;
 
+import android.content.Intent;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.pref_app);
+        setContentView(R.layout.activity_settings_button_layout);
+        toProfileActivity();
     }
+
+
+    private void toProfileActivity() {
+        Button profileScreenButton = (Button)findViewById(R.id.to_profile_button_settings);
+        final Intent intent = new Intent(this, ProfileActivity.class);
+        profileScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
 }
