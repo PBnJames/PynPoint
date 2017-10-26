f359dd7cbd87dd21cbd3f150a274a027d04b1bcb
PBnJames <jamesmmoore@ufl.edu>
2017-10-26 15:48:31 -0400

    Setup profile activity bottom buttons

--- app/src/main/java/com/example/android/pynpoint/ProfileActivity.java
+++ app/src/main/java/com/example/android/pynpoint/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.pynpoint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,49 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        toHistoryActivity();
+        toTimerSetActivity();
+        toSettingsActivity();
+    }
+
+
+    public void toHistoryActivity() {
+        Button historyScreenButton = (Button)findViewById(R.id.to_history_button_profile);
+        final Intent intent = new Intent(this, HistoryActivity.class);
+        historyScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+    public void toTimerSetActivity() {
+        Button timerScreenButton = (Button)findViewById(R.id.to_timer_button_profile);
+        final Intent intent = new Intent(this, TimerSetActivity.class);
+        timerScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void toSettingsActivity() {
+        Button settingsScreenButton = (Button)findViewById(R.id.to_settings_button_profile);
+        final Intent intent = new Intent(this, SettingsActivity.class);
+        settingsScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
