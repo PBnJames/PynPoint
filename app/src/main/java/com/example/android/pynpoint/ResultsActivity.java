4b73609dbf1106d39a56edc2a50cebcf100fd635
PBnJames <jamesmmoore@ufl.edu>
2017-11-16 16:11:25 -0500

    Created Results Activity

---
+++ app/src/main/java/com/example/android/pynpoint/ResultsActivity.java
@@ -0,0 +1,77 @@
+package com.example.android.pynpoint;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    public static Intent buildIntent(Context context){
+        Intent intent = new Intent(context, ResultsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (App.getInstance().isPurpleEnabled()) {
+            setTheme(R.style.ActivityTheme_Primary_Base_Purple);
+        } else if(App.getInstance().isGreenEnabled()){
+            setTheme(R.style.ActivityTheme_Primary_Base_Green);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+
+    public void toProfileActivity() {
+        Button profileScreenButton = (Button)findViewById(R.id.to_profile_button_results);
+        final Intent profileIntent = ProfileActivity.buildIntent(this);
+        profileScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(profileIntent);
+                finish();
+            }
+        });
+    }
+
+    public void toHistoryActivity() {
+        Button historyScreenButton = (Button)findViewById(R.id.to_history_button_results);
+        final Intent historyIntent = HistoryActivity.buildIntent(this);
+        historyScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(historyIntent);
+                finish();
+            }
+        });
+    }
+
+
+    public void toTimerSetActivity() {
+        Button timerScreenButton = (Button)findViewById(R.id.to_timer_button_results);
+        final Intent setTimerIntent = TimerSetActivity.buildIntent(this);
+        timerScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(setTimerIntent);
+                finish();
+            }
+        });
+    }
+
+    public void toSettingsActivity() {
+        Button settingsScreenButton = (Button)findViewById(R.id.to_settings_button_results);
+        final Intent settingsIntent = SettingsActivity.buildIntent(this);
+        settingsScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(settingsIntent);
+                finish();
+            }
+        });
+    }
+}
